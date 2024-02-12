242a059a52bd206af3a4fd1e9462fcdc2d3a5edc
Jimmy Rashid <jimmyrashid22@gmail.com>
2024-02-12 14:51:36 -0800

    Interact with all forms

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 options = webdriver.EdgeOptions()
@@ -16,12 +17,33 @@ driver.implicitly_wait(0.5)
 index = 1
 css_selector = f"#__next > div.base-layout_root__gpH78.globalBodyPadding > div > div > div:nth-child({index}) > div.card-with-buttons_buttonWrapper__dvc9K > button.MuiButtonBase-root.MuiButton-root.commercialOutlined.agent-office-search-card-base_blueButton__unMZ4.MuiButton-text.remax-button_buttonText__saWK7 > span"
 
+# Press contact button
 contact_button = driver.find_element(by=By.CSS_SELECTOR, value=css_selector)
 contact_button.click()
 
+# Enter name
 name_entry = driver.find_element(by=By.NAME, value="name")
 name_entry.send_keys("Kevin Barrett")
-# text_box.send_keys("I'm working ? !")
+
+# Enter email
+email_entry = driver.find_element(by=By.NAME, value="email")
+email_entry.send_keys("kevinbarrett@packbuildings.com")
+
+# Find message box
+message_entry = driver.find_element(by=By.NAME, value="message")
+message_copy = message_entry.text
+words = message_copy.split(" ")[:2]
+message_entry.send_keys(Keys.CONTROL + "a")  # Select all text
+message_entry.send_keys(Keys.DELETE)  # Delete selected text
+
+greeting = " ".join(words)
+message = (
+    greeting
+    + " I am reaching out as the VP of Sales for Pack Buildings. We are a technology powered developer with an innovative new product coming out to service the laneway home space. I would like to buy you a coffee to tell you more about how you can use this to generate more clients and increase the yield on your listings. Feel free to text or email. Have a great day. "
+    + "\n\nKevin Barrett\nkevinbarrett@packbuildings.com\nhttps://www.linkedin.com/in/kevinbarrett76/"
+)
+
+message_entry.send_keys(message)
 
 time.sleep(5)
 
